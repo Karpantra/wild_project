684533061f29157de4ff2b8667fbb6a51922c808
Gautier Weigert <gautier.weigert@gmail.com>
2017-05-23 17:07:32 +0200

    user registration with picture and profil with picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :phone_number, :description)
+      params.require(:user).permit(:name, :email, :phone_number, :description, :photo)
     end
 end
