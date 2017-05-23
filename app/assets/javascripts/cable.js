d6437d1bfa69a8aea90da1290733ded59d22de1e
christophe panon <panon_chr@hotmail.com>
2017-05-23 19:45:49 +0200

    done for the day

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -15,7 +15,7 @@
 
 //smooth effect for homepage link image
 
-$('a[href^=".carousel-top-link "]').click(function(){
+$('a[href^="#carousel-example-generic"]').click(function(){
   var the_id = $(this).attr("href");
   var speed = 850;
   $('html, body').animate({
