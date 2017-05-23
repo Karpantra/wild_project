f4a23ca9875311be72c26af4ea09c47af7f158fd
Gautier Weigert <gautier.weigert@gmail.com>
2017-05-23 11:29:04 +0200

    seed ongoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+
+html_file = open("http://www.louerunlieu.com/tous-les-lieux")
+html_doc = Nokogiri::HTML(html_file)
+
+products = []
+
+#images
+html_doc.search('.wp2imgpreloader').each do |element|
+  title = element.text
+end
+
+#titles
+html_doc.search("").each do |element|
+  image = element.text
+end
