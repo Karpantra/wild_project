6d99d33910df2c1078825f0dda3951fc1033c419
Raphael FOSSE <raphael.fosse91@gmail.com>
2017-05-24 15:54:25 +0200

    add validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_attachments :photos, maximum: 2
+  has_attachments :photos, maximum: 3
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  validates :title, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_attachment :photo
   has_many :bookings
   has_many :products
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :description, presence: true
 
   def self.find_for_facebook_oauth(auth)
   user_params = auth.slice(:provider, :uid)
