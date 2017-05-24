9a00d601aa3a1a0c1c4d72730f93d77972c18d85
Raphael FOSSE <raphael.fosse91@gmail.com>
2017-05-24 16:05:26 +0200

    no validates for desc

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :products
   validates :name, presence: true
   validates :email, presence: true
-  validates :description, presence: true
 
   def self.find_for_facebook_oauth(auth)
   user_params = auth.slice(:provider, :uid)
